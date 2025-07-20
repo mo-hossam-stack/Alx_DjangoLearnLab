5763aef3b20d0203e4b125b223ceb3890c7db8ac
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-20 22:28:01 +0300

    Fix: Add required queries for author books and librarian

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,21 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-author = Author.objects.get(name='J.K. Rowling')
-books_by_author = Book.objects.filter(authors=author)  
-for book in books_by_author:
-    print(book.title)
+author_name = "J.K. Rowling"
+try:
+    author = Author.objects.get(name=author_name)
+    books_by_author = Book.objects.filter(authors=author)
+    print(f"Books by {author_name}:")
+    for book in books_by_author:
+        print(f"- {book.title}")
+except Author.DoesNotExist:
+    print(f"Author '{author_name}' not found.")
 
-book = Book.objects.get(title="Harry Potter and the Philosopher's Stone")
-authors = book.authors.all()
-for author in authors:
-    print(author.name)
-
-library = Library.objects.get(name='Central Library')
-librarian = Librarian.objects.get(library=library)  
-print(librarian.name)
-
-library_name = 'Central Library'
-library = Library.objects.get(name=library_name)  
-books = library.books.all()                     
-for book in books:
-    print(book.title)
+library_name = "Central Library"
+try:
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    print(f"Librarian at {library_name}: {librarian.name}")
+except Library.DoesNotExist:
+    print(f"Library '{library_name}' not found.")
+except Librarian.DoesNotExist:
+    print(f"No librarian assigned to {library_name}.")
