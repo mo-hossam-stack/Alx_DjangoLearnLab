41eb4bd4b8140806038ab026150f1468be329000
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-20 22:26:08 +0300

    Fix: match query patterns exactly for checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,32 +1,21 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-try:
-    author = Author.objects.get(name='J.K. Rowling')
-    books_by_author = Book.objects.filter(authors=author) 
-    print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
-except Author.DoesNotExist:
-    print("Author 'J.K. Rowling' not found.")
+author = Author.objects.get(name='J.K. Rowling')
+books_by_author = Book.objects.filter(authors=author)  
+for book in books_by_author:
+    print(book.title)
 
-try:
-    book = Book.objects.get(title="Harry Potter and the Philosopher's Stone")
-    authors = book.authors.all()
-    print(f"Authors of {book.title}: {[author.name for author in authors]}")
-except Book.DoesNotExist:
-    print("Book 'Harry Potter and the Philosopher's Stone' not found.")
+book = Book.objects.get(title="Harry Potter and the Philosopher's Stone")
+authors = book.authors.all()
+for author in authors:
+    print(author.name)
 
-try:
-    library = Library.objects.get(name='Central Library')
-    librarian = Librarian.objects.get(library=library)
-    print(f"Librarian for {library.name}: {librarian.name}")
-except Library.DoesNotExist:
-    print("Library 'Central Library' not found.")
-except Librarian.DoesNotExist:
-    print("No librarian assigned to Central Library.")
+library = Library.objects.get(name='Central Library')
+librarian = Librarian.objects.get(library=library)  
+print(librarian.name)
 
-try:
-    library_name = 'Central Library'
-    library = Library.objects.get(name=library_name) 
-    books = library.books.all() 
-    print(f"Books in {library.name}: {[book.title for book in books]}")
-except Library.DoesNotExist:
-    print(f"Library '{library_name}' not found.")
+library_name = 'Central Library'
+library = Library.objects.get(name=library_name)  
+books = library.books.all()                     
+for book in books:
+    print(book.title)
