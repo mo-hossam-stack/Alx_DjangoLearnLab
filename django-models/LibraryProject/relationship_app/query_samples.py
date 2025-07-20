b5567bd5069790ea9f0d5c2916b6d8d9ef3c989f
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-20 22:29:45 +0300

    Fix: Ensure required query patterns are explicitly used

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,17 @@
-from relationship_app.models import Author, Book, Library, Librarian
-
-author_name = "J.K. Rowling"
-try:
-    author = Author.objects.get(name=author_name)
-    books_by_author = Book.objects.filter(authors=author)
-    print(f"Books by {author_name}:")
-    for book in books_by_author:
-        print(f"- {book.title}")
-except Author.DoesNotExist:
-    print(f"Author '{author_name}' not found.")
+from relationship_app.models import Library, Book, Author, Librarian
 
 library_name = "Central Library"
-try:
-    library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library)
-    print(f"Librarian at {library_name}: {librarian.name}")
-except Library.DoesNotExist:
-    print(f"Library '{library_name}' not found.")
-except Librarian.DoesNotExist:
-    print(f"No librarian assigned to {library_name}.")
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+for book in books_in_library:
+    print(book.title)
+
+author_name = "J.K. Rowling"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(authors=author)
+for book in books_by_author:
+    print(book.title)
+
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
+print(librarian.name)
