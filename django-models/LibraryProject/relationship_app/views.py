a68e7cd76fb82dfe1da116b69e33d0cac5f319b9
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-25 21:40:14 +0300

    Add logout.html template and fix logout handling

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
-from .models import Library
-from .models import Book
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .models import Library, Book
 
 class LibraryDetailView(DetailView):
     model = Library
@@ -11,3 +11,13 @@ class LibraryDetailView(DetailView):
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
