99f6e8f7452f8905ab2276a522f239ec8c118bbb
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-25 21:42:10 +0300

    fix import for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import Library, Book
