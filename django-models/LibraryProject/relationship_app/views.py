5b0d931e10181f7e2d84be4603af49f102628f6f
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-25 21:17:07 +0300

    fix import for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
+from django.views.generic import DetailView
 from django.shortcuts import render
 from .models import Library
 from .models import Book
-from django.views.generic import DetailView
 
 class LibraryDetailView(DetailView):
     model = Library
