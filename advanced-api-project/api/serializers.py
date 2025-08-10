371118be65b11b63602bee5222a32e21b9b44e0d
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-08-10 13:25:11 +0300

    rename directory as required

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from datetime import date
+from .models import Author, Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)  # related_name='books' from the model
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']
