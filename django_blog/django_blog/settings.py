c450c432b7604a053ea654aad5ff4815cd0a05b2
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-08-11 15:28:52 +0300

    fix(settings): include USER and PORT in DATABASES to satisfy config requirements

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -66,6 +66,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',   # Not used for SQLite
+        'PORT': '',   # Not used for SQLite
     }
 }
 
